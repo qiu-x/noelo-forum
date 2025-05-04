4eb266ded3ecd9f8c1f89994b3fa2e184d745222
Qiu <a5@alexslomka.xyz>
2025-05-04 16:06:38 +0200

    fix file creation order in `createPaths`

--- server/storage/utils.go
+++ server/storage/utils.go
@@ -5,30 +5,45 @@ import (
 	"os"
 )
 
-type FilePath string
-type DirPath string
+func createPaths(dirs []string, files map[string]string) error {
+	if err := createDirs(dirs); err != nil {
+		return err
+	}
+	if err := writeFiles(files); err != nil {
+		return err
+	}
+	return nil
+}
 
-func createPaths(entries map[any]string) error {
+func createDirs(entries []string) error {
+	if entries == nil {
+		return nil
+	}
+
+	for _, path := range entries {
+		if err := os.Mkdir(path, 0755); err != nil {
+			return fmt.Errorf("failed to create dir %s: %w", path, err)
+		}
+	}
+	return nil
+}
+
+func writeFiles(entries map[string]string) error {
+	if entries == nil {
+		return nil
+	}
+
 	for path, content := range entries {
-		switch p := path.(type) {
-		case DirPath:
-			if err := os.Mkdir(string(p), 0755); err != nil {
-				return fmt.Errorf("failed to create dir %s: %w", p, err)
-			}
-		case FilePath:
-			f, err := os.Create(string(p))
-			if err != nil {
-				return fmt.Errorf("failed to create file %s: %w", p, err)
-			}
-			if _, err := f.WriteString(content); err != nil {
-				_ = f.Close()
-				return fmt.Errorf("failed to write to file %s: %w", p, err)
-			}
-			if err := f.Close(); err != nil {
-				return fmt.Errorf("failed to close file %s: %w", p, err)
-			}
-		default:
-			return fmt.Errorf("unsupported path type %T", path)
+		f, err := os.Create(path)
+		if err != nil {
+			return fmt.Errorf("failed to create file %s: %w", path, err)
+		}
+		if _, err := f.WriteString(content); err != nil {
+			_ = f.Close()
+			return fmt.Errorf("failed to write to file %s: %w", path, err)
+		}
+		if err := f.Close(); err != nil {
+			return fmt.Errorf("failed to close file %s: %w", path, err)
 		}
 	}
 	return nil
